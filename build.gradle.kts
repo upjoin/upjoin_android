49eeabfd07719eea60af916f4988c5371a2543f7
Martin Boehmer <boehmer.martin@gmail.com>
2020-07-19 16:40:44 +0200

    0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,6 @@ buildscript {
 
         Plugins.buildscript.forEach { classpath(it) }
 
-        /*
-            classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
-            classpath("com.android.tools.build:gradle:4.0.1")
-            classpath("com.google.gms:google-services:4.3.3")
-            classpath("de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0")
-        */
-
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -28,12 +21,14 @@ allprojects {
     repositories {
         google()
         jcenter()
+
+        apply(plugin = "maven")
+
+        group = "de.upjoin"
     }
+
 }
 
-/* Groovy: task clean(type: Delete) {
-    delete rootProject.buildDir
-}*/
-tasks.register("clean", Delete::class) {
+/*tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
-}
+}*/
